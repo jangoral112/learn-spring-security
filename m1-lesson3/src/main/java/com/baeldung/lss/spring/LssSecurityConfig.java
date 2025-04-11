5cd3f6dabb16348e9106323e6a09ae7eae3aac00
Jan Góral <jan.goral@epassi.com>
2025-04-11 13:45:29 +0200

    M1L3: Config for authorities

--- m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson3/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -13,18 +17,27 @@ public class LssSecurityConfig {
     private PasswordEncoder passwordEncoder;
 
     public LssSecurityConfig(PasswordEncoder passwordEncoder) {
-        super();
         this.passwordEncoder = passwordEncoder;
     }
 
-    //
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requestsConfigurer) -> requestsConfigurer
+            .requestMatchers("/delete/**").hasAnyAuthority("ADMIN", "ADMIN2")
+            .anyRequest().authenticated()
+        );
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
-        auth.
-            inMemoryAuthentication().passwordEncoder(passwordEncoder).
-            withUser("user").password(passwordEncoder.encode("pass")).
-            roles("USER");
-    } // @formatter:on
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(passwordEncoder)
+            .withUser("user").password(passwordEncoder.encode("pass"))
+            .roles("USER");
+    }
 }
